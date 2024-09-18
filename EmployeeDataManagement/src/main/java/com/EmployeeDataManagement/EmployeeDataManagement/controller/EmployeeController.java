5d8b98a7c82359e6f801fe3985d99bf896b49b38
Phuke Shreyas Rao <84406674+Shreyasrao-git@users.noreply.github.com>
2024-09-18 11:58:59 +0530

    Update EmployeeController.java

--- EmployeeDataManagement/src/main/java/com/EmployeeDataManagement/EmployeeDataManagement/controller/EmployeeController.java
+++ EmployeeDataManagement/src/main/java/com/EmployeeDataManagement/EmployeeDataManagement/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api/employee")
 @CrossOrigin(origins = "*")
 public class EmployeeController {
     @Autowired
